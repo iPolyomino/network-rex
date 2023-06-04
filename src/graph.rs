a41b452cda18100e8d14a3dc7f22c059ac72f106
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2023-06-04 10:09:35 +0900

    edge list

--- src/graph.rs
+++ src/graph.rs
@@ -3,10 +3,26 @@ use std::fmt::{Display, Formatter};
 pub struct Graph {
     pub name: String,
     pub nodes: Vec<u32>,
+    pub edges: Vec<(u32, u32)>,
 }
 
 impl Display for Graph {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "Graph: {}", self.name)
+        writeln!(
+            f,
+            "Graph with {} nodes and {} edges",
+            self.nodes.len(),
+            self.edges.len()
+        )
+    }
+}
+
+impl Graph {
+    pub fn edges_list(&self, edge: u32) -> Vec<(u32, u32)> {
+        self.edges
+            .iter()
+            .filter(|&&(x, y)| x == edge || y == edge)
+            .copied()
+            .collect()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     let g = graph::Graph {
         name: String::from("sample"),
         nodes: vec![1, 2, 3],
+        edges: vec![(1, 2), (2, 3)],
     };
     print!("{}", g);
+    for (x, y) in g.edges_list(2) {
+        println!("{} - {}", x, y);
+    }
 }
